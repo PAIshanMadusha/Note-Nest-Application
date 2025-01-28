52563f15c76b66bd7dff7eb687334d547e964a4a
Ishan200207 <168297757+Ishan200207@users.noreply.github.com>
2025-01-28 15:00:33 +0530

    HivePackage and uuidPackage Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:note_nest_application/models/note_model.dart';
+import 'package:note_nest_application/models/todo_model.dart';
 import 'package:note_nest_application/utils/app_router.dart';
 import 'package:note_nest_application/utils/app_theme_data.dart';
 
-void main() {
+void main() async{
+
+  //Hive
+  await Hive.initFlutter();
+
+  //Register
+  Hive.registerAdapter(NoteModelAdapter());
+  Hive.registerAdapter(TodoModelAdapter());
+
+  //Open Hive Boxes
+  await Hive.openBox('notes');
+  await Hive.openBox('todos');
+  
   runApp(MyApp());
 }
 
